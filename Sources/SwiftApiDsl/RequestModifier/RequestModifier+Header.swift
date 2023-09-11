fb285ab8ebc8382f307a1d98734ff7cf7aa583a0
Pierre Mardon <pierre.mardon@gmail.com>
2023-09-11 15:50:47 +0200

    Fix header modifier name

--- Sources/SwiftApiDsl/RequestModifier/RequestModifier+Header.swift
+++ Sources/SwiftApiDsl/RequestModifier/RequestModifier+Header.swift
@@ -2,14 +2,14 @@ import Foundation
 
 public extension RequestModifier {
 
-    static func header(value: String?, headerField: String) -> RequestModifier {
-        .init { $0.setValue(value, forHTTPHeaderField: headerField) }
+    static func header(value: String?, field: String) -> RequestModifier {
+        .init { $0.setValue(value, forHTTPHeaderField: field) }
     }
 }
 
 public extension RequestModifiable {
 
-    func header(value: String?, headerField: String) -> Self {
-        modifier(.header(value: value, headerField: headerField))
+    func header(value: String?, field: String) -> Self {
+        modifier(.header(value: value, field: field))
     }
 }
